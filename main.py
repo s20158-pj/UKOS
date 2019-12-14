f6a90ffed11897e1f138e1e1fec4d01a687eee82
Patryk Klimek <s20158@pjwstk.edu.pl>
2019-12-14 13:42:31 +0100

    trzecia modyfikacja, pierwsza klasa

--- main.py
+++ main.py
@@ -2,6 +2,14 @@
 
 import sys
 def main(argv):
-    print(argv)
+    print('\n'.join(argv))
 
-main(sys.argv)
+class Klasa:
+    def __init__(self, x):
+        self.x = x
+    def string(self, s):
+        return self.x.join(s)
+
+k = Klasa('x')
+print(k.string(sys.argv))
+#main(sys.argv)
